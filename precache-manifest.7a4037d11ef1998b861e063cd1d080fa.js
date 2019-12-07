0badb095266b19427aff83e23fef638f05451cbb
Naresh Kumar <naresh.kr@digiryte.com>
2019-12-07 21:01:02 +0530

    Updates

---
+++ precache-manifest.7a4037d11ef1998b861e063cd1d080fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f86fc505a21e558e81bec88de153a78",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "5f47419e110c1732b9d0",
+    "url": "/profile/static/css/main.17179588.chunk.css"
+  },
+  {
+    "revision": "5688ddb0443fc7854441",
+    "url": "/profile/static/js/2.e6ec4c0c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/profile/static/js/2.e6ec4c0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f47419e110c1732b9d0",
+    "url": "/profile/static/js/main.33a67e90.chunk.js"
+  },
+  {
+    "revision": "e4f35d1a9aad3bd82285",
+    "url": "/profile/static/js/runtime-main.6af78a65.js"
+  }
+]);
